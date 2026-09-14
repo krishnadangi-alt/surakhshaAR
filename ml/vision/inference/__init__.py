46cfdd3181874f8eafb25f8845d092c09a7b33f6
HarshitaJAIN1401 <jainharshita576@gmail.com>
2026-09-14 16:38:25 +0530

    Remove ml/vision folder to focus project on ML competency module

--- ml/vision/inference/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Inference pipelines and thin wrappers around the vision models."""
-
-from .ppe_detector import PPEDetection, PPEDetectionResult, PPEDetector
-
-__all__ = ["PPEDetection", "PPEDetectionResult", "PPEDetector"]
